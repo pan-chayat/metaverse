fea1cbaab14e694ba9609990334d60928124aa1a
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-06-28 23:33:22 +0530

    ludo board shows up yayyy

--- client/src/scenes/Bootstrap.ts
+++ client/src/scenes/Bootstrap.ts
@@ -1,110 +1,118 @@
-import Phaser from 'phaser'
-import Network from '../services/Network'
-import { BackgroundMode } from '../../../types/BackgroundMode'
-import store from '../stores'
-import { setRoomJoined } from '../stores/RoomStore'
+import Phaser from "phaser";
+import Network from "../services/Network";
+import { BackgroundMode } from "../../../types/BackgroundMode";
+import store from "../stores";
+import { setRoomJoined } from "../stores/RoomStore";
 
 export default class Bootstrap extends Phaser.Scene {
-  network!: Network
+  network!: Network;
 
   constructor() {
-    super('bootstrap')
+    super("bootstrap");
   }
 
   preload() {
     this.load.atlas(
-      'cloud_day',
-      'assets/background/cloud_day.png',
-      'assets/background/cloud_day.json'
-    )
-    this.load.image('backdrop_day', 'assets/background/backdrop_day.png')
+      "cloud_day",
+      "assets/background/cloud_day.png",
+      "assets/background/cloud_day.json"
+    );
+    this.load.image("backdrop_day", "assets/background/backdrop_day.png");
     this.load.atlas(
-      'cloud_night',
-      'assets/background/cloud_night.png',
-      'assets/background/cloud_night.json'
-    )
-    this.load.image('backdrop_night', 'assets/background/backdrop_night.png')
-    this.load.image('sun_moon', 'assets/background/sun_moon.png')
+      "cloud_night",
+      "assets/background/cloud_night.png",
+      "assets/background/cloud_night.json"
+    );
+    this.load.image("backdrop_night", "assets/background/backdrop_night.png");
+    this.load.image("sun_moon", "assets/background/sun_moon.png");
 
-    this.load.tilemapTiledJSON('tilemap', 'assets/map/map.json')
-    this.load.spritesheet('tiles_wall', 'assets/map/FloorAndGround.png', {
+    this.load.tilemapTiledJSON("tilemap", "assets/map/map.json");
+    this.load.spritesheet("tiles_wall", "assets/map/FloorAndGround.png", {
       frameWidth: 32,
       frameHeight: 32,
-    })
-    this.load.spritesheet('ludo','assets/map/ludo.png',{
+    });
+    this.load.spritesheet("ludo16x16", "assets/map/ludo16x16.png", {
       frameWidth: 16,
-      frameHeight: 16
-    })
-    this.load.spritesheet('chairs', 'assets/items/chair.png', {
+      frameHeight: 16,
+    });
+    this.load.spritesheet("chairs", "assets/items/chair.png", {
       frameWidth: 32,
       frameHeight: 64,
-    })
-    this.load.spritesheet('computers', 'assets/items/computer.png', {
+    });
+    this.load.spritesheet("computers", "assets/items/computer.png", {
       frameWidth: 96,
       frameHeight: 64,
-    })
-    this.load.spritesheet('whiteboards', 'assets/items/whiteboard.png', {
+    });
+    this.load.spritesheet("whiteboards", "assets/items/whiteboard.png", {
       frameWidth: 64,
       frameHeight: 64,
-    })
-    this.load.spritesheet('vendingmachines', 'assets/items/vendingmachine.png', {
-      frameWidth: 48,
-      frameHeight: 72,
-    })
-    this.load.spritesheet('office', 'assets/items/Modern_Office_Black_Shadow.png', {
+    });
+    this.load.spritesheet(
+      "vendingmachines",
+      "assets/items/vendingmachine.png",
+      {
+        frameWidth: 48,
+        frameHeight: 72,
+      }
+    );
+    this.load.spritesheet(
+      "office",
+      "assets/items/Modern_Office_Black_Shadow.png",
+      {
+        frameWidth: 32,
+        frameHeight: 32,
+      }
+    );
+    this.load.spritesheet("basement", "assets/items/Basement.png", {
       frameWidth: 32,
       frameHeight: 32,
-    })
-    this.load.spritesheet('basement', 'assets/items/Basement.png', {
+    });
+    this.load.spritesheet("generic", "assets/items/Generic.png", {
       frameWidth: 32,
       frameHeight: 32,
-    })
-    this.load.spritesheet('generic', 'assets/items/Generic.png', {
-      frameWidth: 32,
-      frameHeight: 32,
-    })
-    this.load.spritesheet('adam', 'assets/character/adam.png', {
+    });
+    this.load.spritesheet("adam", "assets/character/adam.png", {
       frameWidth: 32,
       frameHeight: 48,
-    })
-    this.load.spritesheet('ash', 'assets/character/ash.png', {
+    });
+    this.load.spritesheet("ash", "assets/character/ash.png", {
       frameWidth: 32,
       frameHeight: 48,
-    })
-    this.load.spritesheet('lucy', 'assets/character/lucy.png', {
+    });
+    this.load.spritesheet("lucy", "assets/character/lucy.png", {
       frameWidth: 32,
       frameHeight: 48,
-    })
-    this.load.spritesheet('nancy', 'assets/character/nancy.png', {
+    });
+    this.load.spritesheet("nancy", "assets/character/nancy.png", {
       frameWidth: 32,
       frameHeight: 48,
-    })
+    });
   }
 
   init() {
-    this.network = new Network()
+    this.network = new Network();
   }
 
   create() {
-    this.launchBackground(store.getState().user.backgroundMode)
+    this.launchBackground(store.getState().user.backgroundMode);
   }
 
   private launchBackground(backgroundMode: BackgroundMode) {
-    this.scene.launch('background', { backgroundMode })
+    this.scene.launch("background", { backgroundMode });
   }
 
   launchGame() {
-    this.network.webRTC?.checkPreviousPermission()
-    this.scene.launch('game', {
+    this.network.webRTC?.checkPreviousPermission();
+    this.scene.launch("game", {
       network: this.network,
-    })
+    });
 
     // update Redux state
-    store.dispatch(setRoomJoined(true))
+    store.dispatch(setRoomJoined(true));
   }
 
   changeBackgroundMode(backgroundMode: BackgroundMode) {
-    this.scene.stop('background')
-    this.launchBackground(backgroundMode)
+    this.scene.stop("background");
+    this.launchBackground(backgroundMode);
   }
 }
--- client/src/scenes/Game.ts
+++ client/src/scenes/Game.ts
@@ -1,143 +1,190 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
 // import { debugDraw } from '../utils/debug'
-import { createCharacterAnims } from '../anims/CharacterAnims'
+import { createCharacterAnims } from "../anims/CharacterAnims";
 
-import Item from '../items/Item'
-import Chair from '../items/Chair'
-import Computer from '../items/Computer'
-import Whiteboard from '../items/Whiteboard'
-import VendingMachine from '../items/VendingMachine'
-import '../characters/MyPlayer'
-import '../characters/OtherPlayer'
-import MyPlayer from '../characters/MyPlayer'
-import OtherPlayer from '../characters/OtherPlayer'
-import PlayerSelector from '../characters/PlayerSelector'
-import Network from '../services/Network'
-import { IPlayer } from '../../../types/IOfficeState'
-import { PlayerBehavior } from '../../../types/PlayerBehavior'
-import { ItemType } from '../../../types/Items'
+import Item from "../items/Item";
+import Chair from "../items/Chair";
+import Computer from "../items/Computer";
+import Whiteboard from "../items/Whiteboard";
+import VendingMachine from "../items/VendingMachine";
+import "../characters/MyPlayer";
+import "../characters/OtherPlayer";
+import MyPlayer from "../characters/MyPlayer";
+import OtherPlayer from "../characters/OtherPlayer";
+import PlayerSelector from "../characters/PlayerSelector";
+import Network from "../services/Network";
+import { IPlayer } from "../../../types/IOfficeState";
+import { PlayerBehavior } from "../../../types/PlayerBehavior";
+import { ItemType } from "../../../types/Items";
 
-import store from '../stores'
-import { setFocused, setShowChat } from '../stores/ChatStore'
+import store from "../stores";
+import { setFocused, setShowChat } from "../stores/ChatStore";
 
 export default class Game extends Phaser.Scene {
-  network!: Network
-  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
-  private keyE!: Phaser.Input.Keyboard.Key
-  private keyR!: Phaser.Input.Keyboard.Key
-  private map!: Phaser.Tilemaps.Tilemap
-  myPlayer!: MyPlayer
-  private playerSelector!: Phaser.GameObjects.Zone
-  private otherPlayers!: Phaser.Physics.Arcade.Group
-  private otherPlayerMap = new Map<string, OtherPlayer>()
-  computerMap = new Map<string, Computer>()
-  private whiteboardMap = new Map<string, Whiteboard>()
+  network!: Network;
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private keyE!: Phaser.Input.Keyboard.Key;
+  private keyR!: Phaser.Input.Keyboard.Key;
+  private map!: Phaser.Tilemaps.Tilemap;
+  myPlayer!: MyPlayer;
+  private playerSelector!: Phaser.GameObjects.Zone;
+  private otherPlayers!: Phaser.Physics.Arcade.Group;
+  private otherPlayerMap = new Map<string, OtherPlayer>();
+  computerMap = new Map<string, Computer>();
+  private whiteboardMap = new Map<string, Whiteboard>();
 
   constructor() {
-    super('game')
+    super("game");
   }
 
   registerKeys() {
-    this.cursors = this.input.keyboard.createCursorKeys()
+    this.cursors = this.input.keyboard.createCursorKeys();
     // maybe we can have a dedicated method for adding keys if more keys are needed in the future
-    this.keyE = this.input.keyboard.addKey('E')
-    this.keyR = this.input.keyboard.addKey('R')
-    this.input.keyboard.disableGlobalCapture()
-    this.input.keyboard.on('keydown-ENTER', (event) => {
-      store.dispatch(setShowChat(true))
-      store.dispatch(setFocused(true))
-    })
-    this.input.keyboard.on('keydown-ESC', (event) => {
-      store.dispatch(setShowChat(false))
-    })
+    this.keyE = this.input.keyboard.addKey("E");
+    this.keyR = this.input.keyboard.addKey("R");
+    this.input.keyboard.disableGlobalCapture();
+    this.input.keyboard.on("keydown-ENTER", (event) => {
+      store.dispatch(setShowChat(true));
+      store.dispatch(setFocused(true));
+    });
+    this.input.keyboard.on("keydown-ESC", (event) => {
+      store.dispatch(setShowChat(false));
+    });
   }
 
   disableKeys() {
-    this.input.keyboard.enabled = false
+    this.input.keyboard.enabled = false;
   }
 
   enableKeys() {
-    this.input.keyboard.enabled = true
+    this.input.keyboard.enabled = true;
   }
 
   create(data: { network: Network }) {
     if (!data.network) {
-      throw new Error('server instance missing')
+      throw new Error("server instance missing");
     } else {
-      this.network = data.network
+      this.network = data.network;
     }
 
-    createCharacterAnims(this.anims)
+    createCharacterAnims(this.anims);
 
-    this.map = this.make.tilemap({ key: 'tilemap' })
-    const FloorAndGround = this.map.addTilesetImage('FloorAndGround', 'tiles_wall')
+    this.map = this.make.tilemap({ key: "tilemap" });
+    const FloorAndGround = this.map.addTilesetImage(
+      "FloorAndGround",
+      "tiles_wall"
+    );
 
-    const groundLayer = this.map.createLayer('Ground', FloorAndGround)
-    groundLayer.setCollisionByProperty({ collides: true })
+    const groundLayer = this.map.createLayer("Ground", FloorAndGround);
+    groundLayer.setCollisionByProperty({ collides: true });
 
     // debugDraw(groundLayer, this)
 
-    this.myPlayer = this.add.myPlayer(705, 500, 'adam', this.network.mySessionId)
-    this.playerSelector = new PlayerSelector(this, 0, 0, 16, 16)
+    this.myPlayer = this.add.myPlayer(
+      705,
+      500,
+      "adam",
+      this.network.mySessionId
+    );
+    this.playerSelector = new PlayerSelector(this, 0, 0, 16, 16);
 
     // import chair objects from Tiled map to Phaser
-    const chairs = this.physics.add.staticGroup({ classType: Chair })
-    const chairLayer = this.map.getObjectLayer('Chair')
+    const chairs = this.physics.add.staticGroup({ classType: Chair });
+    const chairLayer = this.map.getObjectLayer("Chair");
     chairLayer.objects.forEach((chairObj) => {
-      const item = this.addObjectFromTiled(chairs, chairObj, 'chairs', 'chair') as Chair
+      const item = this.addObjectFromTiled(
+        chairs,
+        chairObj,
+        "chairs",
+        "chair"
+      ) as Chair;
       // custom properties[0] is the object direction specified in Tiled
-      item.itemDirection = chairObj.properties[0].value
-    })
+      item.itemDirection = chairObj.properties[0].value;
+    });
 
     // import computers objects from Tiled map to Phaser
-    const computers = this.physics.add.staticGroup({ classType: Computer })
-    const computerLayer = this.map.getObjectLayer('Computer')
+    const computers = this.physics.add.staticGroup({ classType: Computer });
+    const computerLayer = this.map.getObjectLayer("Computer");
     computerLayer.objects.forEach((obj, i) => {
-      const item = this.addObjectFromTiled(computers, obj, 'computers', 'computer') as Computer
-      item.setDepth(item.y + item.height * 0.27)
-      const id = `${i}`
-      item.id = id
-      this.computerMap.set(id, item)
-    })
+      const item = this.addObjectFromTiled(
+        computers,
+        obj,
+        "computers",
+        "computer"
+      ) as Computer;
+      item.setDepth(item.y + item.height * 0.27);
+      const id = `${i}`;
+      item.id = id;
+      this.computerMap.set(id, item);
+    });
 
     // import whiteboards objects from Tiled map to Phaser
-    const whiteboards = this.physics.add.staticGroup({ classType: Whiteboard })
-    const whiteboardLayer = this.map.getObjectLayer('Whiteboard')
+    const whiteboards = this.physics.add.staticGroup({ classType: Whiteboard });
+    const whiteboardLayer = this.map.getObjectLayer("Whiteboard");
     whiteboardLayer.objects.forEach((obj, i) => {
       const item = this.addObjectFromTiled(
         whiteboards,
         obj,
-        'whiteboards',
-        'whiteboard'
-      ) as Whiteboard
-      const id = `${i}`
-      item.id = id
-      this.whiteboardMap.set(id, item)
-    })
+        "whiteboards",
+        "whiteboard"
+      ) as Whiteboard;
+      const id = `${i}`;
+      item.id = id;
+      this.whiteboardMap.set(id, item);
+    });
 
     // import vending machine objects from Tiled map to Phaser
-    const vendingMachines = this.physics.add.staticGroup({ classType: VendingMachine })
-    const vendingMachineLayer = this.map.getObjectLayer('VendingMachine')
+    const vendingMachines = this.physics.add.staticGroup({
+      classType: VendingMachine,
+    });
+    const vendingMachineLayer = this.map.getObjectLayer("VendingMachine");
     vendingMachineLayer.objects.forEach((obj, i) => {
-      this.addObjectFromTiled(vendingMachines, obj, 'vendingmachines', 'vendingmachine')
-    })
+      this.addObjectFromTiled(
+        vendingMachines,
+        obj,
+        "vendingmachines",
+        "vendingmachine"
+      );
+    });
 
     // import other objects from Tiled map to Phaser
-    this.addGroupFromTiled('Wall', 'tiles_wall', 'FloorAndGround', false)
-    this.addGroupFromTiled('Objects', 'office', 'Modern_Office_Black_Shadow', false)
-    this.addGroupFromTiled('ObjectsOnCollide', 'office', 'Modern_Office_Black_Shadow', true)
-    this.addGroupFromTiled('GenericObjects', 'generic', 'Generic', false)
-    this.addGroupFromTiled('GenericObjectsOnCollide', 'generic', 'Generic', true)
-    this.addGroupFromTiled('Basement', 'basement', 'Basement', true)
+    this.addGroupFromTiled("Wall", "tiles_wall", "FloorAndGround", false);
+    this.addGroupFromTiled(
+      "Objects",
+      "office",
+      "Modern_Office_Black_Shadow",
+      false
+    );
+    this.addGroupFromTiled(
+      "ObjectsOnCollide",
+      "office",
+      "Modern_Office_Black_Shadow",
+      true
+    );
+    this.addGroupFromTiled("GenericObjects", "generic", "Generic", false);
+    this.addGroupFromTiled(
+      "GenericObjectsOnCollide",
+      "generic",
+      "Generic",
+      true
+    );
+    this.addGroupFromTiled("Basement", "basement", "Basement", true);
+    this.addGroupFromTiled("Ludo", "ludo16x16", "ludo16x16", false);
 
-    this.otherPlayers = this.physics.add.group({ classType: OtherPlayer })
+    this.otherPlayers = this.physics.add.group({ classType: OtherPlayer });
 
-    this.cameras.main.zoom = 1.5
-    this.cameras.main.startFollow(this.myPlayer, true)
+    this.cameras.main.zoom = 1.5;
+    this.cameras.main.startFollow(this.myPlayer, true);
 
-    this.physics.add.collider([this.myPlayer, this.myPlayer.playerContainer], groundLayer)
-    this.physics.add.collider([this.myPlayer, this.myPlayer.playerContainer], vendingMachines)
+    this.physics.add.collider(
+      [this.myPlayer, this.myPlayer.playerContainer],
+      groundLayer
+    );
+    this.physics.add.collider(
+      [this.myPlayer, this.myPlayer.playerContainer],
+      vendingMachines
+    );
 
     this.physics.add.overlap(
       this.playerSelector,
@@ -145,7 +192,7 @@ export default class Game extends Phaser.Scene {
       this.handleItemSelectorOverlap,
       undefined,
       this
-    )
+    );
 
     this.physics.add.overlap(
       this.myPlayer,
@@ -153,34 +200,38 @@ export default class Game extends Phaser.Scene {
       this.handlePlayersOverlap,
       undefined,
       this
-    )
+    );
 
     // register network event listeners
-    this.network.onPlayerJoined(this.handlePlayerJoined, this)
-    this.network.onPlayerLeft(this.handlePlayerLeft, this)
-    this.network.onMyPlayerReady(this.handleMyPlayerReady, this)
-    this.network.onMyPlayerVideoConnected(this.handleMyVideoConnected, this)
-    this.network.onPlayerUpdated(this.handlePlayerUpdated, this)
-    this.network.onItemUserAdded(this.handleItemUserAdded, this)
-    this.network.onItemUserRemoved(this.handleItemUserRemoved, this)
-    this.network.onChatMessageAdded(this.handleChatMessageAdded, this)
+    this.network.onPlayerJoined(this.handlePlayerJoined, this);
+    this.network.onPlayerLeft(this.handlePlayerLeft, this);
+    this.network.onMyPlayerReady(this.handleMyPlayerReady, this);
+    this.network.onMyPlayerVideoConnected(this.handleMyVideoConnected, this);
+    this.network.onPlayerUpdated(this.handlePlayerUpdated, this);
+    this.network.onItemUserAdded(this.handleItemUserAdded, this);
+    this.network.onItemUserRemoved(this.handleItemUserRemoved, this);
+    this.network.onChatMessageAdded(this.handleChatMessageAdded, this);
   }
 
   private handleItemSelectorOverlap(playerSelector, selectionItem) {
-    const currentItem = playerSelector.selectedItem as Item
+    const currentItem = playerSelector.selectedItem as Item;
     // currentItem is undefined if nothing was perviously selected
     if (currentItem) {
       // if the selection has not changed, do nothing
-      if (currentItem === selectionItem || currentItem.depth >= selectionItem.depth) {
-        return
+      if (
+        currentItem === selectionItem ||
+        currentItem.depth >= selectionItem.depth
+      ) {
+        return;
       }
       // if selection changes, clear pervious dialog
-      if (this.myPlayer.playerBehavior !== PlayerBehavior.SITTING) currentItem.clearDialogBox()
+      if (this.myPlayer.playerBehavior !== PlayerBehavior.SITTING)
+        currentItem.clearDialogBox();
     }
 
     // set selected item and set up new dialog
-    playerSelector.selectedItem = selectionItem
-    selectionItem.onOverlapDialog()
+    playerSelector.selectedItem = selectionItem;
+    selectionItem.onOverlapDialog();
   }
 
   private addObjectFromTiled(
@@ -189,12 +240,17 @@ export default class Game extends Phaser.Scene {
     key: string,
     tilesetName: string
   ) {
-    const actualX = object.x! + object.width! * 0.5
-    const actualY = object.y! - object.height! * 0.5
+    const actualX = object.x! + object.width! * 0.5;
+    const actualY = object.y! - object.height! * 0.5;
     const obj = group
-      .get(actualX, actualY, key, object.gid! - this.map.getTileset(tilesetName).firstgid)
-      .setDepth(actualY)
-    return obj
+      .get(
+        actualX,
+        actualY,
+        key,
+        object.gid! - this.map.getTileset(tilesetName).firstgid
+      )
+      .setDepth(actualY);
+    return obj;
   }
 
   private addGroupFromTiled(
@@ -203,83 +259,115 @@ export default class Game extends Phaser.Scene {
     tilesetName: string,
     collidable: boolean
   ) {
-    const group = this.physics.add.staticGroup()
-    const objectLayer = this.map.getObjectLayer(objectLayerName)
+    const group = this.physics.add.staticGroup();
+    const objectLayer = this.map.getObjectLayer(objectLayerName);
     objectLayer.objects.forEach((object) => {
-      const actualX = object.x! + object.width! * 0.5
-      const actualY = object.y! - object.height! * 0.5
+      const actualX = object.x! + object.width! * 0.5;
+      const actualY = object.y! - object.height! * 0.5;
       group
-        .get(actualX, actualY, key, object.gid! - this.map.getTileset(tilesetName).firstgid)
-        .setDepth(actualY)
-    })
+        .get(
+          actualX,
+          actualY,
+          key,
+          object.gid! - this.map.getTileset(tilesetName).firstgid
+        )
+        .setDepth(actualY);
+    });
     if (this.myPlayer && collidable)
-      this.physics.add.collider([this.myPlayer, this.myPlayer.playerContainer], group)
+      this.physics.add.collider(
+        [this.myPlayer, this.myPlayer.playerContainer],
+        group
+      );
   }
 
   // function to add new player to the otherPlayer group
   private handlePlayerJoined(newPlayer: IPlayer, id: string) {
-    const otherPlayer = this.add.otherPlayer(newPlayer.x, newPlayer.y, 'adam', id, newPlayer.name)
-    this.otherPlayers.add(otherPlayer)
-    this.otherPlayerMap.set(id, otherPlayer)
+    const otherPlayer = this.add.otherPlayer(
+      newPlayer.x,
+      newPlayer.y,
+      "adam",
+      id,
+      newPlayer.name
+    );
+    this.otherPlayers.add(otherPlayer);
+    this.otherPlayerMap.set(id, otherPlayer);
   }
 
   // function to remove the player who left from the otherPlayer group
   private handlePlayerLeft(id: string) {
     if (this.otherPlayerMap.has(id)) {
-      const otherPlayer = this.otherPlayerMap.get(id)
-      if (!otherPlayer) return
-      this.otherPlayers.remove(otherPlayer, true, true)
-      this.otherPlayerMap.delete(id)
+      const otherPlayer = this.otherPlayerMap.get(id);
+      if (!otherPlayer) return;
+      this.otherPlayers.remove(otherPlayer, true, true);
+      this.otherPlayerMap.delete(id);
     }
   }
 
   private handleMyPlayerReady() {
-    this.myPlayer.readyToConnect = true
+    this.myPlayer.readyToConnect = true;
   }
 
   private handleMyVideoConnected() {
-    this.myPlayer.videoConnected = true
+    this.myPlayer.videoConnected = true;
   }
 
   // function to update target position upon receiving player updates
-  private handlePlayerUpdated(field: string, value: number | string, id: string) {
-    const otherPlayer = this.otherPlayerMap.get(id)
-    otherPlayer?.updateOtherPlayer(field, value)
+  private handlePlayerUpdated(
+    field: string,
+    value: number | string,
+    id: string
+  ) {
+    const otherPlayer = this.otherPlayerMap.get(id);
+    otherPlayer?.updateOtherPlayer(field, value);
   }
 
   private handlePlayersOverlap(myPlayer, otherPlayer) {
-    otherPlayer.makeCall(myPlayer, this.network?.webRTC)
+    otherPlayer.makeCall(myPlayer, this.network?.webRTC);
   }
 
-  private handleItemUserAdded(playerId: string, itemId: string, itemType: ItemType) {
+  private handleItemUserAdded(
+    playerId: string,
+    itemId: string,
+    itemType: ItemType
+  ) {
     if (itemType === ItemType.COMPUTER) {
-      const computer = this.computerMap.get(itemId)
-      computer?.addCurrentUser(playerId)
+      const computer = this.computerMap.get(itemId);
+      computer?.addCurrentUser(playerId);
     } else if (itemType === ItemType.WHITEBOARD) {
-      const whiteboard = this.whiteboardMap.get(itemId)
-      whiteboard?.addCurrentUser(playerId)
+      const whiteboard = this.whiteboardMap.get(itemId);
+      whiteboard?.addCurrentUser(playerId);
     }
   }
 
-  private handleItemUserRemoved(playerId: string, itemId: string, itemType: ItemType) {
+  private handleItemUserRemoved(
+    playerId: string,
+    itemId: string,
+    itemType: ItemType
+  ) {
     if (itemType === ItemType.COMPUTER) {
-      const computer = this.computerMap.get(itemId)
-      computer?.removeCurrentUser(playerId)
+      const computer = this.computerMap.get(itemId);
+      computer?.removeCurrentUser(playerId);
     } else if (itemType === ItemType.WHITEBOARD) {
-      const whiteboard = this.whiteboardMap.get(itemId)
-      whiteboard?.removeCurrentUser(playerId)
+      const whiteboard = this.whiteboardMap.get(itemId);
+      whiteboard?.removeCurrentUser(playerId);
     }
   }
 
   private handleChatMessageAdded(playerId: string, content: string) {
-    const otherPlayer = this.otherPlayerMap.get(playerId)
-    otherPlayer?.updateDialogBubble(content)
+    const otherPlayer = this.otherPlayerMap.get(playerId);
+    otherPlayer?.updateDialogBubble(content);
   }
 
   update(t: number, dt: number) {
     if (this.myPlayer && this.network) {
-      this.playerSelector.update(this.myPlayer, this.cursors)
-      this.myPlayer.update(this.playerSelector, this.cursors, this.keyE, this.keyR, this.network)
+      this.playerSelector.update(this.myPlayer, this.cursors);
+      this.myPlayer.update(
+        this.playerSelector,
+        this.cursors,
+        this.keyE,
+        this.keyR,
+        this.network
+      );
     }
   }
 }
