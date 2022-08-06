2ff37f248a0fbd59fc316eb7c2f992f6bd8edece
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-08-06 23:22:00 +0530

    use tokens instead

--- client/src/smartContractInterface/fetchNFTState.ts
+++ client/src/smartContractInterface/fetchNFTState.ts
@@ -2,7 +2,8 @@ import { ABI } from "./abi";
 import { ethers } from "ethers";
 
 export const ContractAddress = "0xe0af15141ABd0B31Fb15e250971936Fe8837230a";
-// export const ContractAddress = "0x58DDA6B8f3A6934fa277077A0b526cf02F34960a";
+export const tokenContractAddress =
+  "0x58DDA6B8f3A6934fa277077A0b526cf02F34960a";
 // const address = "0x6B2eBFe3FE5c5B84746105421de93Df383b222E8";
 const isHolder = async (address: string) => {
   const provider = new ethers.providers.JsonRpcProvider(
@@ -10,8 +11,14 @@ const isHolder = async (address: string) => {
   );
   let contract = new ethers.Contract(ContractAddress, ABI, provider);
   let value = await contract.balanceOf(address);
+
+  const provider_erc = new ethers.providers.JsonRpcProvider(
+    import.meta.env.VITE_ALCHEMY_API
+  );
+  let contract_erc = new ethers.Contract(ContractAddress, ABI, provider_erc);
+  let value_erc = await contract.balanceOf(address);
   console.log("Value is = > ", value.toString());
-  return value > 0 || address === "0x657D3C03e450E4815f3411Aa26713A2A90e9Ad83";
+  return value > 0 || value_erc;
 };
 
 export default isHolder;
